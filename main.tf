b7296236e34cc1b3ff62edd951115c9818de87c3
RobertDuckhamUWS <74392733+RobertDuckhamUWS@users.noreply.github.com>
2023-11-15 16:59:54 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,22 +2,22 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "docker-deploy-test-rg" {
-  name     = "dockerDeployTest"
-  location = "Uk South"
+data "azurerm_resource_group" "docker-deploy-test-rg"{
+  name = "dockerDeployTest"
+  location = "uk south"
 }
 
 resource "azurerm_service_plan" "docker-deploy-test-service-plan" {
   name                = "glasgow-digital-twin-service-plan"
-  resource_group_name = azurerm_resource_group.docker-deploy-test-rg.name
-  location            = azurerm_resource_group.docker-deploy-test-rg.location
+  resource_group_name = data.azurerm_resource_group.docker-deploy-test-rg.name
+  location            = data.azurerm_resource_group.docker-deploy-test-rg.location
   os_type             = "Linux"
   sku_name            = "B1"
 }
 
 resource "azurerm_linux_web_app" "docker-deploy-test" {
   name                = "docker-deploy-test"
-  resource_group_name = azurerm_resource_group.docker-deploy-test-rg.name
+  resource_group_name = data.azurerm_resource_group.docker-deploy-test-rg.name
   location            = azurerm_service_plan.docker-deploy-test-service-plan.location
   service_plan_id     = azurerm_service_plan.docker-deploy-test-service-plan.id
 
