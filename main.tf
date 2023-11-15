56da524dfa6b203131fa12bc207b34d2061071be
RobertDuckhamUWS <74392733+RobertDuckhamUWS@users.noreply.github.com>
2023-11-15 16:25:06 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "docker-deploy-test-rg" {
+  name     = "dockerDeployTest"
+  location = "Uk South"
+}
+
+resource "azurerm_service_plan" "docker-deploy-test-service-plan" {
+  name                = "glasgow-digital-twin-service-plan"
+  resource_group_name = azurerm_resource_group.docker-deploy-test-rg.name
+  location            = azurerm_resource_group.docker-deploy-test-rg.location
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "docker-deploy-test" {
+  name                = "docker-deploy-test"
+  resource_group_name = azurerm_resource_group.docker-deploy-test-rg.name
+  location            = azurerm_service_plan.docker-deploy-test-service-plan.location
+  service_plan_id     = azurerm_service_plan.docker-deploy-test-service-plan.id
+
+  site_config {
+    application_stack {
+      docker_image_name        = "node-hello-world:latest"
+      docker_registry_url      = "https://dockerdeploytestacr.azurecr.io"
+      docker_registry_username = "dockerdeploytestacr"
+    }
+  }
+}
